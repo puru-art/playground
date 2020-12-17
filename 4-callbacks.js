7c3af78e9e7636346a7f9d51042111a2d31ca0d3
puru-art <puru.sewda@mtxb2b.com>
2020-12-17 16:04:53 +0530

    Added new code in 4-callbacks and 8-promises

--- 4-callbacks.js
+++ 4-callbacks.js
@@ -1,33 +1,46 @@
-setTimeout(() => {
-    console.log('Two seconds are up')
-}, 2000)
-
-const names = ['Puru', 'Mayank', 'Meet']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
-})
-
-const geocode = (address, callback) => {
-    setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        };
-        callback(data)
-    },2000)
-}
-
-geocode('Philadelphia', (data) => {
-    console.log(data);
-})
-
-const add = (x, y, callback) => {
-    setTimeout(() => {
-        const sm = x+y;
-        callback(sm)
+const doWorkCallback = (callback) => {
+    setTimeout (() => {
+        //callback('This is my error!', undefined);
+        callback(undefined, [1, 4, 7])
     }, 2000)
 }
+ doWorkCallback((error, result) => {
+    if(error){
+        return console.log(error)
+    }
+    console.log(result);
+ })
 
-add(1, 4, (sum) => {
-    console.log(sum)
-})
+// setTimeout(() => {
+//     console.log('Two seconds are up')
+// }, 2000)
+
+// const names = ['Puru', 'Mayank', 'Meet']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         };
+//         callback(data)
+//     },2000)
+// }
+
+// geocode('Philadelphia', (data) => {
+//     console.log(data);
+// })
+
+// const add = (x, y, callback) => {
+//     setTimeout(() => {
+//         const sm = x+y;
+//         callback(sm)
+//     }, 2000)
+// }
+
+// add(1, 4, (sum) => {
+//     console.log(sum)
+// })
